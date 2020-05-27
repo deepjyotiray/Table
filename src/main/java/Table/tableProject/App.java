5bb946a8e6de57e4fd39862f27a8e557750336db
deep_ray <deepjyotiray2010@gmail.com>
2020-05-27 21:31:12 +0530

    first_commit

---
+++ src/main/java/Table/tableProject/App.java
@@ -0,0 +1,13 @@
+package Table.tableProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
