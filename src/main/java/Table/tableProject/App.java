82d31320b7ea6e4cf8230dc44e6f387c425c2cfd
deep_ray <deepjyotiray2010@gmail.com>
2020-05-28 01:39:09 +0530

    till now

--- src/main/java/Table/tableProject/App.java
+++ src/main/java/Table/tableProject/App.java
@@ -1,5 +1,5 @@
 package Table.tableProject;
-import java.util.ArrayList;
+import java.util.*;
 
 
 
@@ -10,28 +10,54 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        
-        int rownum=0;
-        int colnum=0;
+
         ArrayList<String> column=new ArrayList<String>();
         column.add("ID");
         column.add("Name");
         
-        ArrayList<String> row=new ArrayList<String>();
-        row.add("1");
-        row.add("A");
+        ArrayList<String> row1=new ArrayList<String>();
+        row1.add("1");
+        row1.add("A");
+        
+        ArrayList<String> row2=new ArrayList<String>();
+        row2.add("2");
+        row2.add("B");
+        
+        ArrayList<String> row3=new ArrayList<String>();
+        row3.add("3");
+        row3.add("C");
         
         //System.out.println(column);	
         
         
-        ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>();
+        //ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>();
         
-        table.add(0, column);
-        table.add(1, row);
+        List<ArrayList<String>> table =new ArrayList<>(); 
         
-        System.out.println(table);
+       table.add(column);
+       table.add(row1);
+       table.add(row2);
+       table.add(row3);
+       //table.add(new ArrayList<String>());
+       
+        
+//        List<int[]> rowList = new ArrayList<int[]>();
+//        //table.add(0, column);
+//        //table.add(1, row1);
+//        //table.add(2, row2);
+       
+//        table.add(new ArrayList<String>().add("3"));
+       	System.out.println(table); 
+        //System.out.println(table.get(0));
         
         
+        
+        
+        
+        
+        
+        
+        	
     }
 }
 
